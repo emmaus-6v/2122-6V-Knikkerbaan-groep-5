f067a0062ea33a7a414d4a358fc37a6ca59d0e7b
lars-beep <57898224+lars-beep@users.noreply.github.com>
2022-01-28 19:04:47 +0100

    Add files via upload

---
+++ knikkerbaan_4/knikkerbaan_4.ino
@@ -0,0 +1,16 @@
+
+
+//declareer hier je globale constanten:
+
+const int DRAAI_POORT_PIN = 11;
+const int BOVEN_POORT_PIN = 6;    // pin van servo die bovenste poort regelt
+const int ONDER_POORT_PIN = 10;    // pin van servo die onderste poort regelt
+const int LEEGLOOP_TIJD = 5000;   // tijd die knikkers nodig hebben om na het
+                                  // sluiten van de poort langs alle sensors te komen
+
+char WIFI_NETWERK[] = "ziggo";
+char WIFI_WACHTWOORD[] = "Slagtand197";
+char SERVER_DOMEINNAAM[] = "3000-apricot-cat-jsjadg3z.ws-eu25.gitpod.io";    // domeinnaam van gitpod-server, zoals 3000-lavendel-bla-bla.gitpod.io
+
+const int TELLER_A_PIN = 5;       // pin waarop IR-sensor voor Teller A is aangesloten
+const int TELLER_B_PIN = 3;
